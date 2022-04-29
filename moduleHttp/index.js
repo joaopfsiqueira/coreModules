4677672a331595f3a01470e27538b51cd655dc10
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2022-04-29 08:40:54 -0300

    fix: http.createServer

--- moduleHttp/index.js
+++ moduleHttp/index.js
@@ -6,7 +6,7 @@ const port = 3000;
 //metodo padrão do http para criar o servidor, onde esperamos SEMPRE um req e res.
 // res é mandar a resposta para o cliente. No caso, res.write escreve na tela algo.
 // SEMPRE é necessário usar o end, para não ficar em looping.
-http.createServer((req, res), () => {
+const server = http.createServer((req, res) => {
     res.write("Hello world");
     res.end();
 })
